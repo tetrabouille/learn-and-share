c75ebf95d3926575f2e96723ce9d3f03d7b4a3ef
Thibaut MANDALLENA <tmandallena@squad.fr>
2022-04-03 11:07:13 +0200

    auth store

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -1,1 +1,31 @@
-export default {};
+import { readable } from 'svelte/store';
+import { BehaviorSubject } from 'rxjs';
+import { supabase } from '@/libs/supabase';
+
+const tokenInStorage = localStorage.getItem('token') || null;
+
+const $token = new BehaviorSubject<string | null>(tokenInStorage);
+
+const token = readable(tokenInStorage, (set) => {
+  const sub = $token.subscribe((token) => {
+    set(token);
+  });
+  return () => sub.unsubscribe();
+});
+
+const login = () => {
+  const newToken = supabase.auth.session()?.access_token;
+  console.log(supabase.auth.session());
+
+  if (newToken) {
+    $token.next(newToken);
+    localStorage.setItem('token', newToken);
+  }
+};
+
+const logout = () => {
+  $token.next(null);
+  localStorage.removeItem('token');
+};
+
+export { login, token, logout };
