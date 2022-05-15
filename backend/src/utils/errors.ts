faaeead8ae5d5a3fc719788741f13949ba41d38c
Thibaut MANDALLENA <tmandallena@squad.fr>
2022-05-15 15:08:52 +0200

    add new errors

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -8,15 +8,18 @@ export type UserError = {
 export enum Error {
   // General
   INTERNAL_ERROR = 'INTERNAL_ERROR',
+  INVALID_LANG = 'INVALID_LANG',
+  // Access
+  NOT_REGISTERED = 'NOT_REGISTERED',
   // Forms
   FIELD_REQUIRED = 'FIELD_REQUIRED',
   // User
-  NOT_REGISTERED = 'NOT_REGISTERED',
   USER_ALREADY_EXISTS = 'USER_ALREADY_EXISTS',
   USER_NOT_FOUND = 'USER_NOT_FOUND',
   // Tag
   TAG_ALREADY_EXISTS = 'TAG_ALREADY_EXISTS',
-  INVALID_LANG = 'INVALID_LANG',
+  // Topic
+  TOPIC_ALREADY_EXISTS = 'TOPIC_ALREADY_EXISTS',
 }
 
 export const getMessage = (error: Error) => {
@@ -24,25 +27,30 @@ export const getMessage = (error: Error) => {
     // General
     case Error.FIELD_REQUIRED:
       return 'Missing required fields';
-    // User
+    case Error.INVALID_LANG:
+      return 'Invalid language';
+    // Access
     case Error.NOT_REGISTERED:
       return 'Account not registered';
+    // User
     case Error.USER_ALREADY_EXISTS:
       return 'User already exists';
     case Error.USER_NOT_FOUND:
       return 'User not found';
+    // Topic
+    case Error.TOPIC_ALREADY_EXISTS:
+      return 'Topic already exists';
     // Tag
     case Error.TAG_ALREADY_EXISTS:
       return 'Tag already exists';
-    case Error.INVALID_LANG:
-      return 'Invalid language';
+
     default:
       return 'Unknown error';
   }
 };
 
 export const getError = (model: string) => (errors: Error[]) => {
-  logger.error(errors.join(', '), model);
+  logger.error(errors.map((e) => getMessage(e)).join(', '), model);
   return {
     [model]: null,
     userErrors: errors.map((error) => ({ message: getMessage(error), code: error })),
