139dde40e49f6d1b52e0774b6ea733443285eed4
Thibaut MANDALLENA <tmandallena@squad.fr>
2022-05-23 23:31:37 +0200

    Add InputDate

---
+++ frontend/src/components/forms/InputDate.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import InputWrapper from './InputWrapper.svelte';
+  import { formatDate } from '@/utils/date';
+
+  export let label = null;
+  export let info: string = null;
+  export let placeholder = '...';
+  export let fieldId: string;
+  export let formContextKey = 'form';
+  export let style: 'classic' | 'h1' = 'classic';
+
+  let inputValue = '';
+
+  export const focus = () => {
+    if (inputRef) inputRef.focus();
+  };
+
+  let inputRef: HTMLInputElement;
+
+  $: getClass = () => {
+    const classes = 'w-full focus:outline-none';
+    const ext = (() => {
+      switch (style) {
+        default:
+        case 'classic':
+          return 'rounded-md bg-warm-700/70 px-3 py-1 text-white focus:bg-warm-800/70 placeholder:text-warm-200/70';
+        case 'h1':
+          return 'px-4 pt-2 pb-2 py-1 text-xl bg-creme-50/90 focus:border-b-4 focus:border-b-cold-500 focus:pb-1 placeholder:text-warm-700/70 h-[44px]';
+      }
+    })();
+    return classes.concat(` ${ext}`);
+  };
+
+  const onChange = (e: any, value: string, handleChange: (e: any) => void) => {
+    inputValue = formatDate(value);
+    handleChange(e);
+  };
+
+  $: displayValue = (value: string) => {
+    if (inputRef?.type === 'text') {
+      if (!inputValue) inputValue = formatDate(value);
+      return inputValue;
+    }
+    return value;
+  };
+</script>
+
+<InputWrapper on:input {fieldId} {label} {info} {formContextKey} let:handleChange let:value>
+  <input
+    bind:this={inputRef}
+    id={fieldId}
+    class={getClass()}
+    type="text"
+    {placeholder}
+    value={displayValue(value)}
+    on:focus={() => (inputRef.type = 'date')}
+    on:blur={() => (inputRef.type = 'text')}
+    on:input={(e) => onChange(e, value, handleChange)}
+  />
+</InputWrapper>
---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,18 @@
+import { toDate } from 'date-fns';
+
+const dateFromTimestamp = (value: string) => {
+  const timestamp = Number(value);
+  if (!value || isNaN(timestamp)) return null;
+  console.log(timestamp);
+
+  return toDate(timestamp).toISOString().split('T')[0];
+};
+
+const formatDate = (value: string) => {
+  const date = new Date(value);
+  if (date.toString() === 'Invalid Date') return '';
+
+  return date.toDateString();
+};
+
+export { dateFromTimestamp, formatDate };
