6d4df56070a8ff9eadcac1b509125044cbd987a5
Thibaut MANDALLENA <tmandallena@squad.fr>
2022-06-04 17:06:32 +0200

    WIP - add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+heroku login \
+&& heroku container:login \
+&& for i in {1..5}; 
+    do docker compose -f docker-compose.prod.yml up --build -d && break || sleep 15; 
+    done \
+&& docker tag learn-and-share_prod registry.heroku.com/the-constellation-assets/web \
+&& docker push registry.heroku.com/the-constellation-assets/web
+&& heroku container:release web --app the-constellation-assets
