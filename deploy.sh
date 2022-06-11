127f74b76be7a741414f43c771b9814491047282
Thibaut MANDALLENA <tmandallena@squad.fr>
2022-06-11 10:23:22 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ heroku login \
     do docker compose -f docker-compose.prod.yml up --build -d && break || sleep 15; 
     done \
 && docker tag learn-and-share_prod registry.heroku.com/the-constellation-assets/web \
-&& docker push registry.heroku.com/the-constellation-assets/web
+&& docker push registry.heroku.com/the-constellation-assets/web \
 && heroku container:release web --app the-constellation-assets
