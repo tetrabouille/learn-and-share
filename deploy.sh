855be9fb47c82d8deac3f99592ff510e00b24cac
Thibaut MANDALLENA <tmandallena@squad.fr>
2022-06-24 10:10:38 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-heroku login \
+heroku whoami || heroku login \
 && heroku container:login \
 && for i in {1..5}; 
-    do docker compose -f docker-compose.prod.yml up --build -d && break || sleep 15; 
+    do docker compose -f docker-compose.prod.yml up --build -d && break || sleep 5; 
     done \
+&& docker image rmi registry.heroku.com/the-constellation-assets/web || true \
 && docker tag learn-and-share_prod registry.heroku.com/the-constellation-assets/web \
 && docker push registry.heroku.com/the-constellation-assets/web \
 && heroku container:release web --app the-constellation-assets
